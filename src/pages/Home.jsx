2e2cfbda63df7c24642c33874890baaccdeac42e
Meynard <meynard.trinidad44@gmail.com>
2023-04-13 14:03:36 +0800

    Mini portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import '../assets/css/Home.css';
+import { gsap } from 'gsap';
 
 function Home() {
+
+    const cardRef = useRef();
+
+    useLayoutEffect(() => {
+        gsap.to(cardRef.current, {
+            rotation: '+=360'
+        })
+    });
+
     return (
         <section className="hero">
             <div className="left">
                 <div>
-                    <h3>Meynard Julien B. Trinidad</h3>
-                    <h4>Junior Web Developer</h4>
+                    <h3 className='name'>Meynard Julien B. Trinidad</h3>
+                    <h4 className='position'>Junior Web Developer</h4>
                 </div>
                 <div className='divider'></div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptates ab magnam labore quae adipisci reprehenderit commodi optio pariatur similique! Quod assumenda unde non eaque aperiam reiciendis! Possimus, repellendus hic.</p>
+                    <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptates ab magnam labore quae adipisci reprehenderit commodi optio pariatur similique! Quod assumenda unde non eaque aperiam reiciendis! Possimus, repellendus hic.</p>
                 </div>
             </div>
             <div className="right">
-                <div className="card">
+                <div className="card" ref={cardRef}>
                     <div className='card-overlay'></div>
                     <img className='card-image' src="images/Profile.jpg" alt="Meynard Julien" />
                 </div>
